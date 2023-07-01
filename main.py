0b711f5a83dd0d997fa50200518ac1cc73504cde
0xBAMA <jon.baker1222@gmail.com>
2023-06-30 20:20:06 -0500

    cleanup, formatting, comments, ready to take data

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
 # setup libraries
-import tkinter as tk							# display
-from PIL import Image, ImageChops, ImageTk		# image manip
-import numpy as np								# numerical stuff, for the data array
-import random									# something to get some random numbers
-import time										# something for timer, tbd
+import tkinter as tk						# windowing + display
+from PIL import Image, ImageChops, ImageTk	# image creation, manip, pass to tk
+import numpy as np							# numerical stuff, for the data array
+import random								# something to get some random numbers
 
 class brainScan_t():
+# ================================================================================================
 	def __init__( self ):
+# ================================================================================================
+		# create the window
 		self.root = tk.Tk()
 		self.root.title( "BrainScan" )
 
-		# create a blank image
-		self.dataArray = np.zeros( ( 500, 1000 ) )
+		# data size
+		self.windowWidth = 1000
+		self.windowHeight = 500
+
+		# create a blank PIL Image arrayImage + the corresponding tk ImageTk displayImage
+		self.dataArray = np.zeros( ( self.windowHeight, self.windowWidth ) )
 		self.arrayImage = Image.fromarray( self.dataArray, mode="RGB" )
+		self.displayImage = ImageTk.PhotoImage( self.arrayImage )
 
-		# populate the image with some random data
-		# for x in range( 0, 1000 ):
-		# 	for y in range( 100, 400 ):
-		# 		self.arrayImage.putpixel( ( x, y ), ( random.randint( 0, 255 ), 128, 128 ) )
-
-		self.displayImage = ImageTk.PhotoImage( self.arrayImage ) # create the tk version of it
-
-		# show the image held in img... what is this doing exactly
+		# setup the tkinter label object, to display the ImageTK displayImage
 		self.label = tk.Label( self.root, image = self.displayImage )
-		self.label.pack()
+		self.label.pack() # this is something to do with passing the data, not sure exactly
+
+		# update the data, setup the first update() callback, enter loop
 		self.update()
 		self.root.mainloop()
 
+# ================================================================================================
 	def update( self ):
-		self.arrayImage = ImageChops.offset( self.arrayImage, 1, 0 ) # shift the image to the right
+# ================================================================================================
+		# shift the image one pixel to the right
+		self.arrayImage = ImageChops.offset( self.arrayImage, 1, 0 )
 
-		# write the new pixel data at the leading edge
+		# write the new pixel data at the leading edge - replace random data with spectrogram slice
 		for y in range( 100, 400 ):
-			self.arrayImage.putpixel( ( 0, y ), ( random.randint( 0, 255 ), random.randint( 0, 255 ), random.randint( 0, 255 ) ) );
+			newData = ( random.randint( 0, 255 ), random.randint( 0, 255 ), random.randint( 0, 255 ) )
+			self.arrayImage.putpixel( ( 0, y ), newData )
 
+		# update the contents with the latest data in arrayImage
 		self.displayImage = ImageTk.PhotoImage( self.arrayImage )
 		self.label.configure( image = self.displayImage )
+
+		# register next update callback, 16ms is for 60fps
 		self.root.after( 16, self.update )
 
+# create the object, runs __init__() to initialize, sets up the update callback
 brainScan = brainScan_t()
