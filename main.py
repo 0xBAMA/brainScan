25f5603bdc6cd027196576d54f53e18e84956a5a
0xBAMA <jon.baker1222@gmail.com>
2023-06-30 19:28:47 -0500

    display prototype working

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
 # setup libraries
-import tkinter as tk                            # display
-from PIL import Image, ImageChops, ImageTk      # image manip
-import numpy as np                              # numerical stuff, for the data array
-import random                                   # something to get some random numbers
-import time                                     # something for timer, tbd
+import tkinter as tk							# display
+from PIL import Image, ImageChops, ImageTk		# image manip
+import numpy as np								# numerical stuff, for the data array
+import random									# something to get some random numbers
+import time										# something for timer, tbd
 
 '''
 # I believe this is something workable, to update the image contents
 # https://stackoverflow.com/questions/2400262/how-can-i-schedule-updates-f-e-to-update-a-clock-in-tkinter
 '''
 
-# create the window
-window = tk.Tk()
-window.title( 'BrainScan' )
+class brainScan_t():
+	def __init__( self ):
+		self.root = tk.Tk()
+		self.root.title( "BrainScan" )
 
-# create a blank image
-a = np.zeros( ( 500, 1000 ) )
-arrayImage = Image.fromarray( a, mode="RGB" )
+		# create a blank image
+		self.dataArray = np.zeros( ( 500, 1000 ) )
+		self.arrayImage = Image.fromarray( self.dataArray, mode="RGB" )
 
-# populate the image with some random data
-for x in range( 100, 900 ):
-    for y in range( 100, 400 ):
-        arrayImage.putpixel( ( x, y ), ( random.randint( 0, 255 ), 128, 128 ) )
+		# populate the image with some random data
+		for x in range( 0, 1000 ):
+			for y in range( 100, 400 ):
+				self.arrayImage.putpixel( ( x, y ), ( random.randint( 0, 255 ), 128, 128 ) )
 
-#shift the image to the right
-arrayImage = ImageChops.offset( arrayImage, 100, 0 )
+		self.displayImage = ImageTk.PhotoImage( self.arrayImage ) # create the tk version of it
 
-# create the tk version of it
-displayImage = ImageTk.PhotoImage( arrayImage )
+		# show the image held in img... what is this doing exactly
+		self.label = tk.Label( self.root, image = self.displayImage )
+		self.label.pack()
+		self.update()
+		self.root.mainloop()
 
-# show the image held in img... what is this?
-lbl = tk.Label( window, image = displayImage ).pack()
-# lbl.after( 30, update ) ... there's something with this .after thing, no idea how it works
+	def update( self ):
+		self.arrayImage = ImageChops.offset( self.arrayImage, 1, 0 ) # shift the image to the right
 
-# how to do something in here? tbd
-window.mainloop()
+		# write the new pixel data at the leading edge
+		for y in range( 100, 400 ):
+			self.arrayImage.putpixel( ( 0, y ), ( random.randint( 0, 255 ), random.randint( 0, 255 ), random.randint( 0, 255 ) ) );
 
+
+		self.displayImage = ImageTk.PhotoImage( self.arrayImage )
+		self.label.configure( image = self.displayImage )
+		self.root.after( 16, self.update )
+
+brainScan = brainScan_t()
