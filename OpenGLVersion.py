c10a1ab31c7d8d3a19b2032a59da3cb0d8442742
jb <jon.baker1222@gmail.com>
2023-07-05 19:54:55 -0500

    links to pyopengl docs

---
+++ OpenGLVersion.py
@@ -0,0 +1,4 @@
+# this will be much higher performance than the tkinter version
+
+# https://pyopengl.sourceforge.net/documentation/index.html
+# https://pyopengl.sourceforge.net/context/tutorials/index.html
