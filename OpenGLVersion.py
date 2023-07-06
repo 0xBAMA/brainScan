5eba6bfc429fd3dc2048fde03007878da87c529b
jb <jon.baker1222@gmail.com>
2023-07-06 12:41:26 -0500

    add link to tutorial video set

--- OpenGLVersion.py
+++ OpenGLVersion.py
@@ -2,3 +2,6 @@
 
 # https://pyopengl.sourceforge.net/documentation/index.html
 # https://pyopengl.sourceforge.net/context/tutorials/index.html
+
+# good tutorial set, easy mapping to c++ code
+# https://www.youtube.com/watch?v=LCK1qdp_HhQ&list=PLn3eTxaOtL2PDnEVNwOgZFm5xYPr4dUoR
